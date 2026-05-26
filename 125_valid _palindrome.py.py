615ec38efcbe4d995b6e2f8e24e69092ede6878f
rutuja <rutujamengal26@gmail.com>
2026-05-26 10:06:09 +0530

    Add files via upload

---
+++ 125_valid _palindrome.py.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_s = [char.lower() for char in s if char.isalnum()]
+        left = 0
+        right = len(clean_s) - 1
+
+        while left < right:
+           
+            if clean_s[left] != clean_s[right]: 
+               
+                return False 
+            
+            
+            left += 1
+            right -= 1
+        return True
